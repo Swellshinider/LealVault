a4f593d092c58106926b3596bee8e5157ef41cf3
Swellshinider <eduardoleal.contact@gmail.com>
2025-03-07 13:58:29 -0300

    Add Configuration class for application settings

---
+++ LealVault.Common/Properties/Configuration.cs
@@ -0,0 +1,32 @@
+﻿namespace LealVault.Common.Properties;
+
+/// <summary>
+/// Provides configuration settings for the application.
+/// </summary>
+public static class Configuration
+{
+    private static string? _baseDirectory = null;
+
+    /// <summary>
+    /// Gets the base directory for storing configuration files.
+    /// </summary>
+    public static string BaseDirectory
+    {
+        get
+        {
+            if (_baseDirectory == null)
+            {
+                var folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                _baseDirectory = Path.Combine(folder, "LealVault");
+
+                if (!Directory.Exists(_baseDirectory))
+                    Directory.CreateDirectory(_baseDirectory);
+#if DEBUG
+                Console.WriteLine(_baseDirectory);
+#endif
+            }
+
+            return _baseDirectory;
+        }
+    }
+}
