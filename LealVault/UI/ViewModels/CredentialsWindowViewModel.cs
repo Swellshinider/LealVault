c4274c8a18e5ec5a1c3d692c0e73cb4437f13a0a
Swellshinider <eduardoleal.contact@gmail.com>
2025-03-13 11:48:51 -0300

    some changes

--- LealVault/UI/ViewModels/CredentialsWindowViewModel.cs
+++ LealVault/UI/ViewModels/CredentialsWindowViewModel.cs
@@ -5,6 +5,20 @@ namespace LealVault.UI.ViewModels;
 
 public class CredentialsWindowViewModel : INotifyPropertyChanged
 {
+    private bool _isLogin = true;
+    private string _username = "";
+    private string _password = "";
+
+    public bool IsLogin
+    {
+        get => _isLogin;
+        set
+        {
+            _isLogin = value;
+            OnPropertyChanged(nameof(IsLogin));
+        }
+    }
+
     public event PropertyChangedEventHandler? PropertyChanged;
 
     private void OnPropertyChanged(string propertyName) 
